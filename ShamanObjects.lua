760672c53e59f5e8867ca6c70588f4d93093bee0
Henriquetf <henriquetf2009@gmail.com>
2016-11-05 16:18:09 -0200

    Add list of Shaman objects

---
+++ ShamanObjects.lua
@@ -0,0 +1,145 @@
+ShamanObjects = {
+    -- Padrão
+    Standard {
+        [00] = "Seta",
+        [01] = "Caixa pequena",
+        [02] = "Caixa",
+        [06] = "Bola de praia",
+        [07] = "Trampolim",
+        [10] = "Bigorna",
+        [23] = "Bomba",
+        [24] = "Spirit",
+        [44] = "Totem",
+        [54] = "Gelo",
+        [88] = "Desenhador",
+    }
+
+    -- Tábuas
+    Board {
+        [03] = "Tábua pequena",
+        [04] = "Tábua",
+        [45] = "Tábua de gelo",
+        [46] = "Tábua de chocolate",
+        [60] = "Tábua minúscula",
+        [67] = "Tábua grande",
+    }
+
+    -- Pregos
+    Pin = {
+        [11] = "Prego vermelho",
+        [12] = "Prego vermelho - Girar direita",
+        [13] = "Prego vermelho - Girar esquerda",
+        [14] = "Prego azul - Girar direita",
+        [15] = "Prego azul - Girar esquerda",
+        [16] = "Prego azul - Girar esquerda",
+        [22] = "Prego amarelo",
+    }
+
+    -- Bolas de canhão
+    Cannonball = {
+        [17] = "Bola de canhão - Cima",
+        [18] = "Bola de canhão - Baixo",
+        [19] = "Bola de canhão - Direita",
+        [20] = "Bola de canhão - Esquerda",
+    }
+
+    -- Portais
+    Portal = {
+        [26] = "Portal azul",
+        [27] = "Portal vermelho",
+    }
+
+    -- Balões
+    Balloon = {
+        [28] = "Balão azul",
+        [29] = "Balão vermelho",
+        [30] = "Balão verde",
+        [31] = "Balão amarelo",
+    }
+
+    -- Runas
+    Rune = {
+        [32] = "Runa",
+        [62] = "Runa estável",
+    }
+
+    -- Consumíveis
+    Consumable = {
+        [33] = "Galinha",
+        [34] = "Bola de neve",
+        [35] = "Flecha do amor",
+        [63] = "Peixe",
+        [65] = "Baiacu",
+        [80] = "Avião",
+        [89] = "Abóbora",
+        [90] = "RIP",
+        [95] = "Bola de papel",
+        [96] = "Balão misterioso",
+        [97] = "Energy Orb",
+    }
+
+    -- Habilidades Shaman
+    ShamanSkill = {
+        [36] = "Transformice",
+        [37] = "Teleportador de ratos",
+        [38] = "Reviver",
+        [39] = "Maçã verde",
+        [40] = "Ovelha",
+        [41] = "Remover objeto",
+        [42] = "Mola",
+        [43] = "Bônus de velocidade",
+        [47] = "Dar toca",
+        [55] = "Dar queijo",
+        [56] = "Teleportador Shaman",
+        [57] = "Nuvem",
+        [58] = "Inverter transparência do objeto",
+        [59] = "Bolha",
+        [61] = "Caixa de acompanhamento",
+        [68] = "Triângulo",
+        [69] = "S",
+        [70] = "Teia",
+        [71] = "Rato giratório",
+        [72] = "Remover prego",
+        [73] = "Diminuir rato",
+        [74] = "Folha",
+        [75] = "Limpar sala",
+        [76] = "Boost",
+        [77] = "Prender objetos",
+        [78] = "Reparadora",
+        [79] = "Congelar",
+        [81] = "Remover gravidade",
+        [82] = "Remover gravidade objeto",
+        [83] = "Meep",
+        [84] = "Âncora",
+        [85] = "Rocha(bala de canhão)",
+        [86] = "Fogueira",
+        [92] = "Renovação",
+        [93] = "Puff +salto rato",
+        [94] = "Gatman",
+    }
+
+    -- Nada ou não funciona
+    Broken = {
+        [05] = "Água",
+        [08] = "Água",
+        [09] = "Água",
+        [21] = "Água",
+        [25] = "Queijo", -- Mapa
+        [48] = "Transformice - Corpo caixa pequena",
+        [49] = "Transformice - Corpo caixa grande",
+        [50] = "Transformice - Corpo bigorna",
+        [51] = "Transformice - Corpo tábua pequena",
+        [52] = "Transformice - Corpo tábua grande",
+        [53] = "Editor - Spawn rato",
+        [64] = "Água",
+        [66] = "Prego do balão",
+        [87] = "Gatman Bugado",
+        [91] = "Boneco de neve",
+        [98] = "Água",
+        [99] = "Água",
+        [100] = "Nada - sólido",
+    }
+    
+    -- Em frente há caixas customizadas,
+    -- mas elas não aparecem para outros jogadores
+}
